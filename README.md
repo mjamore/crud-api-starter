ff77615fe6e1a8c2bc875d9b481900d6565737f5
ddcmichaela <michael.amore@coxautoinc.com>
2020-08-16 00:46:02 -0700

    master | Add CRUD routes for posts.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Starter CRUD API
 A lightweight CRUD application that be used to start any project.  It uses Express JS to expose HTTP endpoints that perform CRUD operations on a Mongo database.
 
+## Routes
+|Route|Operation|Description|
+|---|---|---|
+|GET /|N/A|Loads the root route|
+|GET /api|N/A|Loads the /api route|
+|GET /api/posts|Read all|Gets all the posts|
+|GET /api/posts/:id|Read one|Gets one specific post|
+|POST /api/posts/|Create one|Creates a new post|
+|PUT /api/posts/:id|Update one|Updates a specific post|
+|DELETE /api/posts/:id|Delete one|Deletes a specific post|
+
 
 ## Setup
 
--- src/api/posts.js
+++ src/api/posts.js
@@ -2,10 +2,40 @@ const express = require('express');
 
 const router = express.Router();
 
+// Read all
 router.get('/', (req, res) => {
   res.json({
-    message: 'hello from /api/posts'
+    message: 'hello from read all'
   });
 });
+
+// Read one
+router.get('/:id', (req, res) => {
+  res.json({
+    message: 'hello from read one'
+  });
+});
+
+// Create one
+router.post('/', (req, res) => {
+  res.json({
+    message: 'hello from create one'
+  });
+});
+
+// Update one
+router.put('/:id', (req, res) => {
+  res.json({
+    message: 'hello from update one'
+  });
+});
+
+// Delete one
+router.post('/:id', (req, res) => {
+  res.json({
+    message: 'hello from delete one'
+  });
+});
+
 
 module.exports = router;
